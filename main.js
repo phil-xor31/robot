37ed86450ceee15b6b847a393336353358882616
Gloria Adams <thetripler@Glorias-MacBook-Air.local>
2014-06-22 11:27:34 -0700

    UL changes to HTML CSS added py folder

--- main.js
+++ main.js
@@ -1,4 +1,36 @@
-$function () {
+
+$(function () {
+// Set init state to Free Form
+('.btn').addClass('.freefm');
+
+
+('#menu').on('change', function() {
+
+	('btn').removeClass('')
+
+});
+
+('#fwd').on('click', function() {
+	// AJAX calls in here
+
+});
+
+('#back').on('click', function() {
+	// AJAX calls in here
+	
+});
+
+('#left').on('click', function() {
+	// AJAX calls in here
+	
+});
+
+('#right').on('click', function() {
+	// AJAX calls in here
+	
+});
+
+
 
 
 
@@ -14,4 +46,4 @@ $function () {
 
 
 	
-}
+});
