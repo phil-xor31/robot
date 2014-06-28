7d2e260d4b09eeddf9c924474c9ed71690b80a3a
dynamo3 <dynamo3@users.noreply.github.com>
2014-06-27 19:46:40 -0700

    Hooked py to buttons

--- main.js
+++ main.js
@@ -2,94 +2,92 @@
 
 $(function () {
 
-var motion = 'motion';
-
-$.ajaxSetup({
-		type: 'POST',
-		dataType: 'JSON',
-		cache: false
-	});
 
 
-$('#fwd').on('click', function() {
-	// AJAX calls in here
-	console.log("Forward was clicked");
+	var motion = 'motion';
 
-	$.ajax({
-		url: 'roboCtrl.php',
-		type: 'POST',
-		dataType: 'TEXT',
-		cache: false,
-		data: {motion: 'fwd'},
-		success: function(returnData) {
-			// just expecting a string to be returned for debugging only
-			console.log("success");
-			console.log(returnData);
-		}
-	});
-
-}); //end forward
-
-$('#back').on('click', function() {
-	// AJAX calls in here
-	console.log("Back was clicked");
-
-	$.ajax({
-		url: 'roboCtrl.php',
-		type: 'POST',
-		dataType: 'TEXT',
-		cache: false,
-		data: {motion: 'back'},
-		success: function(returnData) {
-			// just expecting a string to be returned for debugging only
-			console.log("success");
-			console.log(returnData);
-		}
-	});
-}); // end back
+	$.ajaxSetup({
+			type: 'POST',
+			dataType: 'JSON',
+			cache: false
+		});
 
 
-$('#left').on('click', function() {
-	// AJAX calls in here
-	console.log("Left was clicked");
-	$.ajax({
-		url: 'roboCtrl.php',
-		type: 'POST',
-		dataType: 'TEXT',
-		cache: false,
-		data: {motion: 'left'},
-		success: function(returnData) {
-			// just expecting a string to be returned for debugging only
-			console.log("success");
-			console.log(returnData);
-		}
-	});
-}); // end left
-});
+	$('#fwd').on('click', function() {
+		// AJAX calls in here
+		console.log("Forward was clicked");
 
-$('#right').on('click', function() {
-	// AJAX calls in here
-	console.log("Right was clicked");
-	$.ajax({
-		url: 'roboCtrl.php',
-		type: 'POST',
-		dataType: 'TEXT',
-		cache: false,
-		data: {motion: 'left'},
-		success: function(returnData) {
-			// just expecting a string to be returned for debugging only
-			console.log("success");
-			console.log(returnData);
-		}
-	});
-}); // end right
-//});
+		$.ajax({
+			url: 'roboCtrl.php',
+			type: 'POST',
+			dataType: 'TEXT',
+			cache: false,
+			data: {motion: 'fwd'},
+			success: function(returnData) {
+				// just expecting a string to be returned for debugging only
+				console.log("success");
+				console.log(returnData);
+			}
+		});
+
+	}); //end forward
+
+	$('#back').on('click', function() {
+		// AJAX calls in here
+		console.log("Back was clicked");
+
+		$.ajax({
+			url: 'roboCtrl.php',
+			type: 'POST',
+			dataType: 'TEXT',
+			cache: false,
+			data: {motion: 'back'},
+			success: function(returnData) {
+				// just expecting a string to be returned for debugging only
+				console.log("success");
+				console.log(returnData);
+			}
+		});
+	}); // end back
 
 
+	$('#left').on('click', function() {
+		// AJAX calls in here
+		console.log("Left was clicked");
+		$.ajax({
+			url: 'roboCtrl.php',
+			type: 'POST',
+			dataType: 'TEXT',
+			cache: false,
+			data: {motion: 'left'},
+			success: function(returnData) {
+				// just expecting a string to be returned for debugging only
+				console.log("success");
+				console.log(returnData);
+			}
+		});
+	}); // end left
+
+	$('#right').on('click', function() {
+		// AJAX calls in here
+		console.log("Right was clicked");
+		$.ajax({
+			url: 'roboCtrl.php',
+			type: 'POST',
+			dataType: 'TEXT',
+			cache: false,
+			data: {motion: 'left'},
+			success: function(returnData) {
+				// just expecting a string to be returned for debugging only
+				console.log("success");
+				console.log(returnData);
+			}
+		});
+	}); // end right
 
 
 	
-});
+}); //end of file
 
 
 
--- roboCtrl.php
+++ roboCtrl.php
@@ -4,18 +4,22 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
 {
 	if ($_POST['motion'] == 'fwd')
 	{
+		exec('sudo python python/rrForward.py');
 		echo 'Forward Received';
 	}
 	else if ($_POST['motion'] == 'back')
 	{
+		exec('sudo python python/rrBack.py');
 		echo 'Backward Received';
 	}
 	else if ($_POST['motion'] == 'left')
 	{
+		exec('sudo python python/rrLeft.py');
 		echo 'Left Received';
 	}
 	else if ($_POST['motion'] == 'right')
 	{
+		exec('sudo python python/rrRight.py');
 		echo 'Right Received';
 	}
 
