49b709a9205b24360ff5c05b13166e299bf6f452
dynamo3 <dynamo3@users.noreply.github.com>
2014-06-27 19:07:56 -0700

    finished code for dropdown

--- menu.js
+++ menu.js
@@ -1,29 +1,37 @@
 $(function() {
 
-var readyMade = function () {
-	$('.rdy').show();
-};
-
-var ownPattern = function () {
-	$('.ownptn').show();
-};
-
-
 // This unhides the menu option readyMade button.
-// Is supposed to figure out WHICH <option> was selected tho!
-$('#select').on('click', function() {
+	// Is supposed to figure out WHICH <option> was selected.
+	// .on('change') will make choice right away wo needing to choose menu <select>ion,
+	// w a button (but I WANT to use a btn).
+	$('#select').on('click', function() {
 
-	console.log("readymade pattern clicked");
+		var selection = $('#menu1').find(':selected').val();
+		//alert(selection + " clicked");
 
-	$("#menu").change(function() {
-		alert("Im in the change inactive to active");
+		$('#trySome').addClass('inactive');
+		$('#trySome').removeClass('active');
 
-		('.inactive .rdy').addClass('active');
-		('.inactive .rdy').removeClass('inactive');
-		$('.rdy').show();
+
+		if (selection == "free") {
+			$('#ff_border').addClass('active');
+			$('#ff_border').removeClass('inactive');
+			console.log("freeform done");
+		}
+
+		if (selection == "ready") {
+			$('#rdy').addClass('active');
+			$('#rdy').removeClass('inactive');
+			console.log("ready done");
+		}
+
+		if(selection == "own") {
+			$('#ownptn').addClass('active');
+			$('#ownptn').removeClass('inactive');
+			console.log("own done");
+		}
 
 	});
-});
 
 
 
